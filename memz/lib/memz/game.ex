94a787157559197a14d29eb6a03c36cd5cd7dadd
Raul Pereira <raul.pereira@gmail.com>
2023-03-04 22:22:26 -0300

    v1: fix boundaries

--- memz/lib/memz/game.ex
+++ memz/lib/memz/game.ex
@@ -9,7 +9,7 @@ defmodule Memz.Game do
     __struct__(text: text, steps: steps)
   end
 
-  def create(%{valid: true} = changeset) do
+  def create(%{valid?: true} = changeset) do
     Eraser.new(changeset.changes.text, changeset.changes.steps)
   end
 
